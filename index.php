60efb24b9e010f0e280632d5608ff593ec6648f0
arjunseshadri1 <31937278+arjunseshadri1@users.noreply.github.com>
2017-11-05 12:04:36 -0500

    Add files via upload

--- index.php
+++ index.php
@@ -1,37 +1,105 @@
 <?php
-$host = "sql2.njit.edu";
-$username = "as3395";
-$password = "Ilw3YILr3";
-$db = "as3395";
 
-try {
-    $connection = new PDO("mysql:host=$host;dbname=$db", $username, $password);
-      echo "Connected successfully"."<br>";
-      $sql="select * from accounts where id<6";
-      $query = $connection->query($sql); 
-      $query->setFetchMode(PDO::FETCH_ASSOC);
-      echo $query->rowCount();
-      
-      echo '<table border="1">';
-        while($r = $query->fetch())
-        {
-            echo "<tr>";
-                echo "<td>" . htmlspecialchars($r['id']) ."</td>";
-                echo "<td>" . htmlspecialchars($r['email']) ."</td>";
-                echo "<td>" . htmlspecialchars($r['fname']) ."</td>";
-                echo "<td>" . htmlspecialchars($r['lname']) ."</td>";
-                echo "<td>" . htmlspecialchars($r['phone']) ."</td>";
-                echo "<td>" . htmlspecialchars($r['birthday']) ."</td>";
-                echo "<td>" . htmlspecialchars($r['gender']) ."</td>";
-                echo "<td>" . htmlspecialchars($r['password']) ."</td>";
-            echo "</tr>";      
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+
+class Manage {
+
+    public static function autoload($class) {
+
+        include $class . '.php';
+    }
+
+}
+
+spl_autoload_register(array('Manage', 'autoload'));
+
+$obj = new main();
+
+class main {
+
+    public function __construct() {
+        $pageRequest = 'homepage';
+
+        if (isset($_REQUEST['page'])) {
+            $pageRequest = $_REQUEST['page'];
         }
-      
-      echo "</table>";
-      
+
+        $page = new $pageRequest;
+
+        $page->post();
     }
-catch(PDOException $e)
-    {
-      echo $e->getMessage()."<br>";
+
+}
+
+abstract class page {
+
+    protected $html;
+
+    public function __construct() {
+        $this->html .= '<html>';
+        $this->html .= '<link rel="stylesheet" href="layout.css">';
+        $this->html .= '<body>';
     }
+
+    public function __destruct() {
+        $this->html .= '</body></html>';
+        stringFunctions::printThis($this->html);
+    }
+
+    public function post() {
+        print_r($_POST);
+    }
+
+}
+
+class homepage extends page {
+
+    public function post() {
+        require_once './DBConst.php';
+        try {
+            $connection = new PDO("mysql:host=" . HOST . ";" . "dbname=" . DB, USERNAME, PASSWORD);
+            $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+            $connection->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+            $this->html .= "<p>Connected Successfully <br>";
+            $sql = "SELECT * FROM accounts WHERE id<6";
+            $q = $connection->query($sql);
+            $q->setFetchMode(PDO::FETCH_ASSOC);
+            $this->html .= "<p>";
+            $this->html.=$q->rowCount();
+            $this->html .= "</p>";
+            $this->html .= '<table border="1">';
+            while ($r = $q->fetch()) {
+                $this->html .= "<tr>";
+                $this->html .= "<td>" . htmlspecialchars($r['id']) . "</td>";
+                $this->html .= "<td>" . htmlspecialchars($r['email']) . "</td>";
+                $this->html .= "<td>" . htmlspecialchars($r['fname']) . "</td>";
+                $this->html .= "<td>" . htmlspecialchars($r['lname']) . "</td>";
+                $this->html .= "<td>" . htmlspecialchars($r['phone']) . "</td>";
+                $this->html .= "<td>" . htmlspecialchars($r['birthday']) . "</td>";
+                $this->html .= "<td>" . htmlspecialchars($r['gender']) . "</td>";
+                $this->html .= "<td>" . htmlspecialchars($r['password']) . "</td>";
+                $this->html .= "</tr>";
+            }
+            $this->html .= '</table>';
+        } catch (PDOException $e) {
+            echo $e->getMessage() . $this->html .= "<br>";
+        }
+    }
+
+}
+
+class stringFunctions {
+
+    static public function printThis($inputText) {
+        return print($inputText);
+    }
+
+    static public function stringLength($text) {
+        return strLen($text);
+    }
+
+}
+
 ?>
